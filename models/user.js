110656a5bc0fad01780a4666e8e8c81565742bb7
KarrynS <karrynsaw@gmail.com>
2021-02-27 19:47:51 +1100

    adding diet column

--- models/user.js
+++ models/user.js
@@ -46,10 +46,9 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.STRING,
       allowNUll: false
     },
-    vegetarian: {
-      type: DataTypes.BOOLEAN,
+    diet: {
+      type: DataTypes.STRING,
       allowNull: false,
-      defaultValue: false
     }
   });
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
