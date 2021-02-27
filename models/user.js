1b6e47ee5e1906a85640217021a9e2c581e80b3a
KarrynS <karrynsaw@gmail.com>
2021-02-27 20:29:51 +1100

    updating package.json

--- models/user.js
+++ models/user.js
@@ -48,7 +48,7 @@ module.exports = function(sequelize, DataTypes) {
     },
     diet: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: false
     }
   });
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
