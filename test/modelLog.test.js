6ecf0245c6da28db98e6b108f0489fb77b61beb6
lenny <llaska@live.com>
2021-03-04 20:53:00 +1100

    base testing files, added jest to dependencies and jest script function to run jest test as test runs eslint

---
+++ test/modelLog.test.js
@@ -0,0 +1,22 @@
+const dailylogTest = require("../models/dailylog");
+const indexTest = require("../models/index");
+const userTest = require("../models/user");
+
+
+test("Can set office number via constructor argument", () => {
+  const testValue = 100;
+  const e = new Manager("Jack", 1, "test@test.com", testValue);
+  expect(e.officeNumber).toBe(testValue);
+});
+
+test('getRole() should return "Manager"', () => {
+  const testValue = "Manager";
+  const e = new Manager("Jack", 1, "test@test.com", 100);
+  expect(e.getRole()).toBe(testValue);
+});
+
+test("Can get office number via getOffice()", () => {
+  const testValue = 100;
+  const e = new Manager("Jack", 1, "test@test.com", testValue);
+  expect(e.getOfficeNumber()).toBe(testValue);
+});
