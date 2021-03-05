ffe14277343cc22403672fe035f5444db0ac0e83
Blake Sticland <blakelists@yahoo.com.au>
2021-03-05 11:55:25 +1100

    fetch call returning response for Spoonacular API; returns recipe results based on diet; next need to add the returned data to the html and handlebars

--- config/spoonacularAPI/spoonacularAPI.js
+++ config/spoonacularAPI/spoonacularAPI.js
@@ -1,42 +1,84 @@
 "use strict";
 
-const API_APP_KEY2 = "&apiKey=74f87b20cfd04c5aa17c1f09d672b5fa";
-const API_APP_KEY4 = "?apiKey=12379bbb78244c0189010a85deb6a8e3"; // third API key for times when 402 error is returned
+const getRecipes = () => {
+  // const recipesReturned = {
+  //   firstName: "John",
+  //   lastName: "Doe",
+  //   age: 50,
+  //   eyeColor: "blue",
+  //   beard: "YESSSSIRRRREE"
+  // };
 
-const API_PATH = "https://api.spoonacular.com/recipes/complexSearch";
-const API_PATH2 = "https://api.spoonacular.com/recipes/";
-// eslint-disable-next-line no-unused-vars
-const recipesReturned = [];
+  const fetch = require("node-fetch");
 
-// Gets a bunch of recipes matching the keyword searched.
-const apiUrl = API_PATH;
-const apiUrlExtension = "?diet=" + "vegan"; // this is where we need the result of the diet dropdown entered
-const apiNumberOfResults = "&number=4";
-apiUrl += apiUrlExtension + apiNumberOfResults + API_APP_KEY2;
+  const API_APP_KEY2 = "&apiKey=74f87b20cfd04c5aa17c1f09d672b5fa";
+  const API_APP_KEY4 = "&apiKey=12379bbb78244c0189010a85deb6a8e3"; // second API key for times when 402 error is returned
+  const API_PATH = "https://api.spoonacular.com/recipes/complexSearch";
+  const API_PATH2 = "https://api.spoonacular.com/recipes/informationBulk";
+  // eslint-disable-next-line no-unused-vars
+  // const recipesReturned = [];
+  // Gets a bunch of recipes matching the keyword searched.
+  const apiUrlExtension = "?diet=" + "vegan"; // this is where we need the result of the diet dropdown entered
+  const apiNumberOfResults = "&number=4";
+  const apiUrl = API_PATH + apiUrlExtension + apiNumberOfResults + API_APP_KEY2;
+  let id;
+  let recipesReturned;
+  // const apiUrlExtension2 = "/information";
+  const apiUrlExtension3 = "&includeNutrition=true";
+  let recipeIds = "";
+  const apiUrlExtensionRecipes = "?ids=";
 
-const apiUrl2 = API_PATH2;
-let id;
-const apiUrlExtension2 = "/information";
-const apiUrlExtension3 = "&includeNutrition=true";
-const query = apiUrl2 + id + apiUrlExtension2 + API_APP_KEY4 + apiUrlExtension3;
+  fetch(apiUrl, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+    .then(data => data.json())
+    .then(response => {
+      for (let i = 0; i < response.results.length; i++) {
+        id = response.results[i].id;
+        if (i < response.results.length - 1) {
+          recipeIds += `${id},`;
+        } else {
+          recipeIds += `${id}`;
+        }
+      }
+      bulkRecipeQuery();
+    })
+    .catch(err => console.error(err));
 
-fetch(apiUrl).then(response => {
-  for (let i = 0; i < response.results.length; i++) {
-    id = response.results[i].id;
-    console.log(id);
-    console.log(query);
-    $.ajax({
-      url: query,
-      method: "GET"
-    }).then(response => {
-      console.log(response.title);
-      console.log(response.image);
-      console.log(response.spoonacularSourceUrl);
-      recipesReturned.push(response);
+  const bulkRecipeQuery = () => {
+    const apiUrl2 =
+      API_PATH2 +
+      apiUrlExtensionRecipes +
+      recipeIds +
+      apiUrlExtension3 +
+      API_APP_KEY4;
 
-      module.exports = recipesReturned;
-    });
-  }  // get recipes for 
-});
-// -------
-// to do change responses to objects to be called instead of console logged.
+    fetch(apiUrl2, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(data => data.json())
+      .then(response => {
+        recipesReturned = response;
+        for (let i = 0; i < response.length; i++) {
+          console.log(response[i].title);
+          console.log(response[i].image);
+          console.log(response[i].id);
+          console.log(response[i].spoonacularSourceUrl);
+          console.log(response[i].nutrition.nutrients[0].name);
+          console.log(response[i].nutrition.nutrients[0].amount);
+          console.log(response[i].nutrition.nutrients[0].unit);
+          // recipesReturned.push(response);
+        }
+      })
+      .catch(err => console.error(err));
+    return recipesReturned;
+  };
+};
+
+module.exports = { getRecipes };
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,6 +1,7 @@
 // Requiring path to so we can use relative routes to our HTML files
 // const path = require("path");
 const isAuthenticated = require("../config/middleware/isAuthenticated");
+const { getRecipes } = require("../config/spoonacularAPI/spoonacularAPI");
 const { myFunc } = require("../config/spoonacularAPI/test");
 module.exports = function(app) {
   // routing for the signup page
@@ -20,14 +21,21 @@ module.exports = function(app) {
   // User not logged will be redirected to the signup page
   app.get("/members", isAuthenticated, (req, res) => {
     const test = myFunc();
-    console.log(test);
-    console.log(isAuthenticated);
+    const test2 = getRecipes();
+    console.log(test2);
+    // console.log(test2.firstName);
+    // console.log(test2.lastName);
+    // console.log(test2.age);
+    // console.log(test2.eyeColor);
+    // console.log(test2.beard);
+    // console.log(isAuthenticated);
     if (isAuthenticated) {
       console.log(req.user);
       return res.render("members", {
         style: "members.css",
         name: req.user.name,
         four: test
+        // sixty: test2.beard
       });
     }
     return res.render("signup", {
