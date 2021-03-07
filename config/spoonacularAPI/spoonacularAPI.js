ab4b701d4d555fe9de65fbc8b33ef56aece4991b
Blake Sticland <blakelists@yahoo.com.au>
2021-03-07 19:51:06 +1100

    spoonacular response is being passed through to html-routes

--- config/spoonacularAPI/spoonacularAPI.js
+++ config/spoonacularAPI/spoonacularAPI.js
@@ -1,110 +1,74 @@
 "use strict";
 
-const { json, response } = require("express");
-const util = require('util');
+const fetch = require("node-fetch");
+const API_APP_KEY2 = "&apiKey=74f87b20cfd04c5aa17c1f09d672b5fa";
+const API_APP_KEY4 = "&apiKey=12379bbb78244c0189010a85deb6a8e3"; // second API key for times when 402 error is returned
+const API_PATH = "https://api.spoonacular.com/recipes/random"; // first API call to get random recipes
+const API_PATH2 = "https://api.spoonacular.com/recipes/informationBulk"; // second API call to get information about recipes
+const apiUrlExtension3 = "&includeNutrition=true";
+const apiUrlExtensionRecipes = "?ids=";
 
-const getRecipes = () => {
-  // function test2Func() {
-  //   const test2 = {
-  //     firstName: "John",
-  //     lastName: "Doe",
-  //     age: 50,
-  //     eyeColor: "blue",
-  //     beard: "YESSSSIRRRREE"
-  //   };
-  //   return test2;
-  // }
-
-  const fetch = require("node-fetch");
-
-  const API_APP_KEY2 = "&apiKey=74f87b20cfd04c5aa17c1f09d672b5fa";
-  const API_APP_KEY4 = "&apiKey=12379bbb78244c0189010a85deb6a8e3"; // second API key for times when 402 error is returned
-  const API_PATH = "https://api.spoonacular.com/recipes/random"; // first API call to get random recipes
-  const API_PATH2 = "https://api.spoonacular.com/recipes/informationBulk"; // second API call to get information about recipes
-  // eslint-disable-next-line no-unused-vars
-  // Gets a bunch of recipes matching the keyword searched.
-  const apiUrlExtension = "?tags=" + "vegan"; // this is where we need the result of the diet dropdown entered
-  const apiNumberOfResults = "&number=4";
-  const apiUrl = API_PATH + apiUrlExtension + apiNumberOfResults + API_APP_KEY2;
-  let id;
-  // let recipesReturned = [];
-  // const apiUrlExtension2 = "/information";
-  const apiUrlExtension3 = "&includeNutrition=true";
-  const apiUrlExtensionRecipes = "?ids=";
-
-  async function getRecipeIds() {
-    try {
-      const data = await fetch(apiUrl, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json"
-        }
-      });
-      const response = await data.json();
-      let recipeIds = "";
-      for (let i = 0; i < response.recipes.length; i++) {
-        id = response.recipes[i].id;
-        if (i < response.recipes.length - 1) {
-          recipeIds += `${id},`;
-        } else {
-          recipeIds += `${id}`;
-        }
-      }
-      return recipeIds;
-    } catch (err) {
-      console.error(err);
-    }
-  }
-
-  getRecipeIds();
-
-  async function bulkRecipeQuery() {
-    try {
-      const recipeIds = await getRecipeIds();
-      const apiUrl2 =
-        API_PATH2 +
-        apiUrlExtensionRecipes +
-        recipeIds +
-        apiUrlExtension3 +
-        API_APP_KEY4;
-      const data = await fetch(apiUrl2, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json"
-        }
-      });
-      const response = await data.json();
-      // recipesReturned = response;
-      // for (let i = 0; i < response.length; i++) {
-      // console.log(response[i].title);
-      // console.log(response[i].image);
-      // console.log(response[i].id);
-      // console.log(response[i].spoonacularSourceUrl);
-      // console.log(response[i].nutrition.nutrients[0].name);
-      // console.log(response[i].nutrition.nutrients[0].amount);
-      // console.log(response[i].nutrition.nutrients[0].unit);
-      // recipesReturned.push(response[i]);
-      // }
-      const loggedResponse = console.log(
-        "This is the logged response: " + response
-      );
-      return response;
-    } catch (err) {
-      console.error(err);
-    }
-  }
-  bulkRecipeQuery();
-};
-
-async function getRecipesAsync() {
+async function getRecipeIds() {
   try {
-    const res = await getRecipes();
-    console.log("This is the response at bottom of js file: " + res);
+    const apiUrlExtension = "?tags=" + "vegan"; // this is where we need the result of the diet dropdown entered
+    const apiNumberOfResults = "&number=10";
+    const apiUrl = API_PATH + apiUrlExtension + apiNumberOfResults + API_APP_KEY2;  
+    const data = await fetch(apiUrl, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+    const response = await data.json();
+    let recipeIds = "";
+    for (let i = 0; i < response.recipes.length; i++) {
+      const id = response.recipes[i].id;
+      if (i < response.recipes.length - 1) {
+        recipeIds += `${id},`;
+      } else {
+        recipeIds += `${id}`;
+      }
+    }
+    return recipeIds;
   } catch (err) {
     console.error(err);
   }
-  // return recipeRetrieval;
 }
-getRecipesAsync();
+
+const getRecipes = async () => {
+  try {
+    const recipeIds = await getRecipeIds();
+    const apiUrl2 =
+      API_PATH2 +
+      apiUrlExtensionRecipes +
+      recipeIds +
+      apiUrlExtension3 +
+      API_APP_KEY4;
+    const data = await fetch(apiUrl2, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+    const response = await data.json();
+    // recipesReturned = response;
+    // for (let i = 0; i < response.length; i++) {
+    // console.log(response[i].title);
+    // console.log(response[i].image);
+    // console.log(response[i].id);
+    // console.log(response[i].spoonacularSourceUrl);
+    // console.log(response[i].nutrition.nutrients[0].name);
+    // console.log(response[i].nutrition.nutrients[0].amount);
+    // console.log(response[i].nutrition.nutrients[0].unit);
+    // recipesReturned.push(response[i]);
+    // }
+    console.log(
+    "This is the logged response: " + response
+    );
+    return response;
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 module.exports = { getRecipes };
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,7 +2,6 @@
 // const path = require("path");
 const isAuthenticated = require("../config/middleware/isAuthenticated");
 const { getRecipes } = require("../config/spoonacularAPI/spoonacularAPI");
-const { myFunc } = require("../config/spoonacularAPI/test");
 module.exports = function(app) {
   // routing for the signup page
   app.get("/", (req, res) => {
@@ -20,30 +19,54 @@ module.exports = function(app) {
   // Authenticated middleware on this route.
   // User not logged will be redirected to the signup page
   app.get("/members", isAuthenticated, (req, res) => {
-    const test = myFunc();
-    // getRecipes();
-    async function waitForRecipes() {
-      try {
-        const response = await getRecipes();
-        console.log("This is the html-routes test2 response: " + response);
-        // responseTitle;
-        return response;
-      } catch (err) {
-        console.error(err);
-      }
-    }
-    waitForRecipes();
-    console.log("This is the html-routes test response: " + test);
-    // for (let i = 0; i < test2.response.length; i++) {
-    //   console.log(test2[i].title);
-    //   console.log(response[i].image);
-    //   console.log(response[i].id);
-    //   console.log(response[i].spoonacularSourceUrl);
-    //   console.log(response[i].nutrition.nutrients[0].unit);
-    //   console.log(response[i].nutrition.nutrients[0].name);
-    //   console.log(response[i].nutrition.nutrients[0].amount);
-    //   // recipesReturned.push(response);
+    getRecipes().then(function(response) {
+      console.log(JSON.stringify(response[0].summary));
+    })
+    // const fruties = ['apple', 'orange', 'kiwi']
+
+    // let transformed = []
+    // for (let index = 0; index < fruiies.length; index++) {
+    //   const element = fruiies[index];
+    //   transformed.push('a')
+      
     // }
+    // const fruit2 = fruities.map((fruit) => {
+    //   return 'a'
+    // })
+
+    // getRecipes().then(function(response) {
+
+    //   return response.map((item) => {
+    //     return {
+    //       title: item.title,
+
+    //     }
+    //   })
+    // getRecipes().then(function(response){
+    //   return response.map(item => {
+    //     return {
+    //       recipeTitle: response.title,
+    //       // recipeImage: response.image,
+    //       // recipeId: response.id,
+    //       // recipeUrl: response.url,
+    //       // recipeNutrientsUnit: response.nutrition.nutrients[0].unit,
+    //       // recipeNutrientsName: response.nutrition.nutrients[0].name,
+    //       // recipeNutrientsAmount: response.nutrition.nutrients[0].amount,
+    //     }
+    //   })
+    // })
+    //   for (let i = 0; i < response.length; i++) {
+    //     console.log(response[i].title);d
+    //     console.log(response[i].image);
+    //     console.log(response[i].id);
+    //     console.log(response[i].spoonacularSourceUrl);
+    //     console.log(response[i].nutrition.nutrients[0].unit);
+    //     console.log(response[i].nutrition.nutrients[0].name);
+    //     console.log(response[i].nutrition.nutrients[0].amount);
+    //     recipesReturned.push(response);
+    //   }
+    //   return response;
+    // });
     console.log(isAuthenticated);
     if (isAuthenticated) {
       console.log(req.user);
