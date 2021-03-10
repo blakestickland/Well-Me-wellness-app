24fc0f8a847721350d6c19d59b271acba568e109
Blake Sticland <blakelists@yahoo.com.au>
2021-03-10 22:08:17 +1100

    graph displaying four bars per day from user data in db

--- public/js/graph.js
+++ public/js/graph.js
@@ -1,17 +1,20 @@
 $(document).ready();
 
-const graphInfo = () => {
+const createGraph = () => {
   fetch("/api/graph")
     .then(response => {
       return response.json();
     })
     .then(results => {
       console.log("Graph.js", results);
+      // Get an example of the createdAt data.
+      console.log("Graph.js", results[0].createdAt);
       const dailyScore = [];
       const waterIntake = [];
       const exercise = [];
       const calorieIntake = [];
       const sleepHour = [];
+      const daysOfWeek = [];
 
       // const dailyScorearr = results;
       for (let i = 0; i < results.length; i++) {
@@ -20,95 +23,60 @@ const graphInfo = () => {
         exercise.push(results[i].exercise);
         calorieIntake.push(results[i].calories);
         sleepHour.push(results[i].sleep);
+        daysOfWeek.push(moment(results[i].createdAt).format("dddd"));
       }
-      console.log(dailyScore);
-      console.log(waterIntake);
-      console.log(exercise);
-      console.log(calorieIntake);
-      console.log(sleepHour);
-      // Object.keys(results).map(function(key, index) {
-      //   results[dailyScore]
-      // }
+
       // createGraph(results);
-    });
-};
-graphInfo();
 
-//const createGraph = () => {
-const ctx = document.getElementById("weekly-result");
-// eslint-disable-next-line no-unused-vars
-const myChart = new Chart(ctx, {
-  type: "bar",
-  data: {
-    labels: [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday"
-    ],
-    datasets: [
-      {
-        label: "Water Intake",
-        backgroundColor: "#3e95cd",
-        data: [7, 6, 5, 5, 6, 7, 5]
-      },
-      {
-        label: "Calorie Intake",
-        backgroundColor: "#8e5ea2",
-        data: [4, 6, 8, 5, 6, 4, 6]
-      },
-      {
-        label: "Excercise",
-        backgroundColor: "#3cba9f",
-        data: [6, 5, 4, 5, 6, 7, 9]
-      },
-      {
-        label: "Sleep",
-        backgroundColor: "#e8c3b9",
-        data: [7, 6, 6, 7, 8, 8, 7]
-      }
-    ]
-  },
-
-  options: {
-    title: {
-      display: true,
-      text: "Self Score (Rating 1-10)"
-    }
-  },
-  options: {
-    scales: {
-      yAxes: [
-        {
-          ticks: {
-            beginAtZero: true
+      // const createGraph = () => {
+      const ctx = document.getElementById("weekly-result");
+      // eslint-disable-next-line no-unused-vars
+      const myChart = new Chart(ctx, {
+        type: "bar",
+        data: {
+          labels: daysOfWeek,
+          datasets: [
+            {
+              label: "Water Intake",
+              backgroundColor: "#3e95cd",
+              data: waterIntake
+            },
+            {
+              label: "Calorie Intake",
+              backgroundColor: "#8e5ea2",
+              data: calorieIntake
+            },
+            {
+              label: "Excercise",
+              backgroundColor: "#3cba9f",
+              data: exercise
+            },
+            {
+              label: "Sleep",
+              backgroundColor: "#e8c3b9",
+              data: sleepHour
+            }
+          ]
+        },
+        options: {
+          title: {
+            display: true,
+            text: "Self Score (Rating 1-10)"
+          }
+        },
+        options: {
+          scales: {
+            yAxes: [
+              {
+                ticks: {
+                  beginAtZero: true
+                }
+              }
+            ]
           }
         }
-      ]
-    }
-  }
-});
-//};
+      });
+    });
+};
 
-// const date = new Date();
-// date.setDate(date.getDate() + 7);
-
-// const query = dailylogs.findAll ({
-//   where: {
-//     userId: 1
-//     createdAt: {
-//       $gt:
-//       $lt: //end of week
-//     }
-//   }
-//   order: {
-//     createdAt: "asc"
-//   }
-//   raw: true,
-//   nested: true,
-// })
-
-// yourModelResults.json
+createGraph();
