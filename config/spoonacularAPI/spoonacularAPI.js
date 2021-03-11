e4b803b3983e8af8a261341bebdc7231a52cd604
Blake Sticland <blakelists@yahoo.com.au>
2021-03-11 15:42:07 +1100

    switched to API key 3 while doing last tests and presentation prep

--- config/spoonacularAPI/spoonacularAPI.js
+++ config/spoonacularAPI/spoonacularAPI.js
@@ -1,8 +1,8 @@
 "use strict";
 
 require("dotenv").config();
-const API_APP_KEY2 = "&apiKey=" + process.env.API_KEY5; // API keys for Spoonacular; limited to 150 points a day.
-const API_APP_KEY4 = "&apiKey=" + process.env.API_KEY5; // second API key to not overload the first one with hits.
+const API_APP_KEY2 = "&apiKey=" + process.env.API_KEY3; // API keys for Spoonacular; limited to 150 points a day.
+const API_APP_KEY4 = "&apiKey=" + process.env.API_KEY3; // second API key to not overload the first one with hits.
 const API_PATH = "https://api.spoonacular.com/recipes/random"; // first API call to get random recipes
 const API_PATH2 = "https://api.spoonacular.com/recipes/informationBulk"; // second API call to get information about recipes
 const apiUrlExtension = "?tags="; // this is where we need the result of the diet dropdown entered
